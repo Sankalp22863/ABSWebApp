08f2f153d79327bdb839034f3daec45d34ebc4f6
Sankalp Naik <51851043+Sankalp22863@users.noreply.github.com>
2021-10-30 13:43:51 +0530

    Feature of prediction of the Bandwidth using LSTM.

--- main.py
+++ main.py
@@ -223,10 +223,12 @@ df = pd.DataFrame({'Ping':[],
 st.title('Adaptive Bit-rate Scheme.')
 
 # Create a page dropdown 
-page = st.sidebar.selectbox("Choose your page", ["View Lecture.", "Future Bandwidth prediction."]) 
+page = st.sidebar.selectbox("Choose your page", ["View Video.", "Future Bandwidth prediction."]) 
 
 
-if page == "View Lecture.":
+if page == "View Video.":
+	st.sidebar.markdown("Bandwidth monitoring Web App built using Streamlit")
+	st.sidebar.markdown(" ")
 
 	# Displaying the df.
 
@@ -277,39 +279,237 @@ if page == "View Lecture.":
 		thread5.join()
 		thread6.join()
 
+# Importing the Necessary Libraries.
+import math
+import tensorflow.compat.v1 as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.layers import Dense,Dropout
 
+# Functions.
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=10):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+# preparing the dataset.
+def prep_data(df, look_back):
+	df = pd.DataFrame(df['download'].to_list())
+	dataset = df.values
+	dataset = dataset.astype('float32')
+	# normalize the dataset
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset = scaler.fit_transform(dataset)
+	# split into train and test sets
+	train_size = int(len(dataset) * 0.8)
+	test_size = len(dataset) - train_size
+	train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+	# print(len(train), len(test))
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
+	# reshape input to be [samples, time steps, features]
+	trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+	testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+	return trainX, trainY, testX, testY, scaler, dataset
+
+def generate_model(hidd_layers, hidd_nodes, look_back):
+	model = Sequential()
+
+	# Adding the first LSTM layer and some Dropout regularisation.
+	model.add(LSTM(units = hidd_nodes, return_sequences = True, input_shape = (1, look_back)))
+	model.add(Dropout(0.2))
+
+	for i in range(hidd_layers - 1):
+		# Adding a hidden LSTM layer and some Dropout regularisation.
+		model.add(LSTM(units = hidd_nodes, return_sequences = True))
+		model.add(Dropout(0.2))
+	# Adding a fourth LSTM layer and some Dropout regularisation
+	model.add(LSTM(units = hidd_nodes))
+	model.add(Dropout(0.2))
+
+	# Adding the output layer
+	model.add(Dense(units = 1))
+
+	# Compiling the RNN
+	model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+	return model
+
+def predict(model, trainX, testX, trainY, testY, scaler):
+	# make predictions
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+	# invert predictions
+	trainPredict = scaler.inverse_transform(trainPredict)
+	trainY = scaler.inverse_transform([trainY])
+	testPredict = scaler.inverse_transform(testPredict)
+	testY = scaler.inverse_transform([testY])
+	# calculate root mean squared error
+	trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+	# print('Train Score: %.2f RMSE' % (trainScore))
+	testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+	# print('Test Score: %.2f RMSE' % (testScore))
+	col1, col2 = st.columns(2)
+
+	col1.metric(label = "Train Score", value = "%.2f"%trainScore)
+	col2.metric(label = "Test Score", value = "%.2f"%testScore)
+	
+	return trainPredict, testPredict
+	
+def disp_res(trainPredict, testPredict, dataset, look_back):
+	# shift train predictions for plotting
+	trainPredictPlot = np.empty_like(dataset)
+	trainPredictPlot[:, :] = np.nan
+	trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+	# shift test predictions for plotting
+	testPredictPlot = np.empty_like(dataset)
+	testPredictPlot[:, :] = np.nan
+	testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+	# plot baseline and predictions
+	fig = plt.figure()
+	# Create 1 3D subplot:
+	ax = fig.add_subplot(111)
+	ax.plot(scaler.inverse_transform(dataset))
+	ax.plot(trainPredictPlot)
+	ax.plot(testPredictPlot)
+	ax.set_xlabel('Time')
+	ax.set_ylabel('Bandwidth(Mbps)')
+	# ax.title('Model Performance'))
+	st.pyplot(fig)
+	return
+
+# Diaplaying the Progress to the User.
+
+class CustomCallback(keras.callbacks.Callback):
+	def __init__(self, no_of_epochs):
+		self.progress = 0
+		self.disp = ''
+		self.pred = ''
+		self.prog_disp = st.progress(self.progress)
+		self.no_of_epochs = no_of_epochs
+		return
+	
+	def on_train_begin(self, logs=None):
+		self.disp = st.markdown("The Training has been initiated.")
+		return
+		
+	def on_train_end(self, logs=None):
+		self.disp.markdown("The Training Process has Ended.")
+		return
+	
+	def on_epoch_begin(self, epoch, logs=None):
+		self.progress += 1/self.no_of_epochs
+		try:
+			self.prog_disp.progress(self.progress)
+		except:
+			self.prog_disp.progress(1.0)
+		# st.progress(self.progress)
+		return 
+	
 
 if page == "Future Bandwidth prediction.":
-	data = read_data('speedtest.log')
-	data = data[6500:7000]
-	data = data.fillna(0)
+
+	st.sidebar.markdown("Future Bandwidth prediction Web App built using Streamlit")
+	st.sidebar.markdown(" ")
+
+	# Choose the number of the hidden nodes.
+	hidd_layers = st.sidebar.number_input('Choose the Number of the hidden Layers', min_value = 1, max_value = 5, value = 3)
+	# Choose the Number of hidden nodes.
+	hidd_nodes = st.sidebar.number_input('Select the Number of the Hidden Nodes', min_value = 10, max_value = 100, value = 50, step = 10)
+	# Choose the Memory of the system.
+	look_back = st.sidebar.number_input('Select the Memory of the system', min_value = 5, max_value = 15, value = 10)
+	# Choose the Number of Epochs.
+	no_of_epochs = st.sidebar.number_input('Select the No. of Epochs you have to train the model for', min_value = 50, max_value = 150, value = 100, step = 10)
 	# st.dataframe(data)
-	time = st.sidebar.slider('Hours of Prediction:', 1, 4)
-	period = time * 60
+	# time = st.sidebar.slider('Hours of Prediction:', 1, 4)
+	# period = time * 60
 
-	draw = st.line_chart(data['download'])
+	# importing the Data to work with.
 
-	# Predict forecast with Prophet.
-	df_train = data[['timestamp', 'download']]
-	st.sidebar.dataframe(df_train)
-	df_train.rename(columns = {'timestamp':'ds', 'download':'y'}, inplace = True)
-	# df.columns = ['ds', 'y']
-	df_train.dropna()
+	choose_dataset = st.sidebar.radio("Choose a dataset to work with :", ["Airtel Wifi Dataset.", "Mumbai Wifi Rajesh Dataset.", "Upload Your own Dataset."])
 
-	m = Prophet()
-	m.fit(df_train)
-	future = m.make_future_dataframe(periods=period)
-	forecast = m.predict(future)
+	# file_upload = st.sidebar.checkbox("Use Your Own Log File for precition.")
+	file_upload = False
+	if choose_dataset == "Upload Your own Dataset.":
+		file_upload = True
+	wrong_format = not file_upload
 
-	# Show and plot forecast
-	st.subheader('Forecast data')
-	st.write(forecast.tail())
+	if file_upload:
+		try:
+			uploaded_file = st.sidebar.file_uploader("Choose a file", ['.log', '.csv'])
+			if uploaded_file is not None:
+				data = read_data(uploaded_file)
+				data = data.fillna(0)
+				draw = st.line_chart(data['download'])
+			else:
+				st.sidebar.error("Please Choose a file.")
+		except:
+			st.sidebar.error("The Format of the file uploaded is wrong")
+			wrong_format = True	
+	if file_upload and wrong_format:
+		data = read_data('Mobile_Data_4G_Airtel.log')
+		data = data.fillna(0)
+		draw = st.line_chart(data['download'])
+	
+	if choose_dataset == "Airtel Wifi Dataset.":
+		data = read_data('Mobile_Data_4G_Airtel.log')
+		data = data.fillna(0)
+		draw = st.line_chart(data['download'])
+	elif choose_dataset == "Mumbai Wifi Rajesh Dataset.":
+		data = read_data('speedtest.log')
+		data = data[6500:]
+		data = data.fillna(0)
+		draw = st.line_chart(data['download'])
+	
+	# Start the Computation.
+	compute = st.sidebar.checkbox('Start Training the Model.')
+
+	if compute:
+		trainX, trainY, testX, testY, scaler, dataset = prep_data(data, look_back)
+
+		model = generate_model(hidd_layers, hidd_nodes, look_back)
+		# Training the Data.
+		with st.spinner(text='Model Training in progress'):
+			model.fit(trainX, trainY, epochs=no_of_epochs, batch_size=32, verbose=1, callbacks=[CustomCallback(no_of_epochs)])
+			st.balloons()
+			st.success("Model Trained Succesfully.")
+		disp = st.markdown("Displaying the Resutls.")
+		trainPredict, testPredict = predict(model, trainX, testX, trainY, testY, scaler)
+		disp_res(trainPredict, testPredict, dataset, look_back)
+		disp.markdown("Results")
+
+
+	# # Predict forecast with Prophet.
+	# df_train = data[['timestamp', 'download']]
+	# st.sidebar.dataframe(df_train)
+	# df_train.rename(columns = {'timestamp':'ds', 'download':'y'}, inplace = True)
+	# # df.columns = ['ds', 'y']
+	# df_train.dropna()
+
+	# m = Prophet()
+	# m.fit(df_train)
+	# future = m.make_future_dataframe(periods=period)
+	# forecast = m.predict(future)
+
+	# # Show and plot forecast
+	# st.subheader('Forecast data')
+	# st.write(forecast.tail())
 		
-	st.write(f'Forecast plot for {time} hours')
-	fig1 = plot_plotly(m, forecast)
-	st.plotly_chart(fig1)
+	# st.write(f'Forecast plot for {time} hours')
+	# fig1 = plot_plotly(m, forecast)
+	# st.plotly_chart(fig1)
 
-	st.write("Forecast components")
-	fig2 = m.plot_components(forecast)
-	st.write(fig2)
+	# st.write("Forecast components")
+	# fig2 = m.plot_components(forecast)
+	# st.write(fig2)
 
